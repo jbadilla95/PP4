35c32e2810481256db612f5f48813649b99e9ff8
Jeison badilla <j.jeison94@hotmail.com>
2019-11-12 19:15:32 -0600

    1st

---
+++ PP4/PP4.UI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP4.UI
+{
+    public class Class1
+    {
+    }
+}
